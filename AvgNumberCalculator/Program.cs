556603ee080e620ded4fb07477db274a259b67b9
Krystof Pavlis <Krystof.Pavlis@education.nsw.gov.au>
2017-11-22 10:54:39 +1100

    Created DiceSimulator and AvgNumberCalculator

---
+++ AvgNumberCalculator/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AvgNumberCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("AVERAGE CALCULATOR");
+            Console.WriteLine("---------------------");
+            Console.Write("Please enter your numbers, separated by spaces: ");
+            string number = Console.ReadLine();
+            string[] numbers = number.Split(' ');
+            float sum = 0;
+
+            foreach (string num in numbers)
+            {
+                sum += float.Parse(num);
+            }
+      
+            float avg = sum / numbers.Length;
+
+            Console.Clear();
+            Console.WriteLine("Calculating...");
+            Thread.Sleep(1100);
+            Console.Clear();
+            Console.WriteLine($"The average of your numbers is {avg}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DiceSimulator/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DiceSimulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("DICE SIMULATOR");
+            Console.WriteLine("--------------------");
+            Console.Write("Choose amount of dice to roll: ");
+            int dice = int.Parse(Console.ReadLine());
+            Console.Clear();
+            Console.WriteLine($"Rolling dice...");
+            Thread.Sleep(1000);
+            Console.Clear();
+
+            Random rnd = new Random();
+            int total = rnd.Next(dice, dice * 6 + 1);
+
+            Console.WriteLine($"Your {dice} dice rolled a total of {total}!");
+            Console.ReadKey();
+        }
+    }
+}
